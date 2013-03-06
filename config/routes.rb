86bb13fe3b4c179694079a168df1f38d691c693c
José Mota <jose@josemota.net>
2013-03-06 18:18:54 +0000

    Update app to accomodate foundation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails3demo::Application.routes.draw do
   resources :images
 
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -51,7 +50,7 @@ Rails3demo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'images#index'
 
   # See how all your routes lay out with "rake routes"
 
