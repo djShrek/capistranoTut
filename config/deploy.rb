d80406b3536fe33889f4e4d2a71eee5994ca86bd
José Mota <jose@josemota.net>
2013-03-07 11:04:21 +0000

    Configure deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,16 @@
 require 'bundler/capistrano'
 load 'deploy/assets'
+
 set :application, "rails3demo"
+set :bundle_flags, "--deployment --quiet --binstubs"
+set :default_environment, {
+    'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
+set :rails_env, :production
+set :deploy_to, "/home/www/apps/rails3demo"
 set :repository,  "git://github.com/josemota/rails3demo"
 set :user, "www"
 set :use_sudo, false
-set :deploy_to, "/home/www/apps/rails3demo"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -25,3 +31,9 @@ server "192.168.1.82", :web, :app, :db, primary: true
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+namespace :deploy do
+  task :start do
+    run "cd #{deploy_to}/current; bundle exec rails s -d"
+  end
+end
