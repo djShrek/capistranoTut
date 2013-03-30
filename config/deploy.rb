1e6732394d607754af2bfe74cfdb5a203fddd769
José Mota <jose@josemota.net>
2013-03-30 11:28:20 +0000

    Checkpoint: Lesson 10

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,13 @@
-require 'bundler/capistrano'
-load 'deploy/assets'
-
 set :application, "rails3demo"
-set :bundle_flags, "--deployment --quiet --binstubs"
-set :default_environment, {
-    'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
-}
-set :rails_env, :production
-set :deploy_to, "/home/www/apps/rails3demo"
 set :repository,  "git://github.com/josemota/rails3demo"
-set :user, "www"
+set :user, "vagrant"
+set :deploy_to, "/home/vagrant/apps/rails3demo"
 set :use_sudo, false
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-server "192.168.1.82", :web, :app, :db, primary: true
+server "192.168.1.93", :app, :web, :db, primary: true
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -31,9 +23,3 @@ server "192.168.1.82", :web, :app, :db, primary: true
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
-
-namespace :deploy do
-  task :start do
-    run "cd #{deploy_to}/current; bundle exec rails s -d"
-  end
-end
